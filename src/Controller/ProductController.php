bb3a15eb7abf304ba0be427efc26949d877667a7
kronhyx <kronhyx@gmail.com>
2020-06-09 13:24:52 -0300

    refactor: add customizable abstract controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ProductController
+ * @package App\Controller
+ */
 class ProductController extends AbstractController
 {
     /**
      * @Route("/product")
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
