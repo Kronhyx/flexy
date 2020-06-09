f923b76cc727618e157fddaf47f1e35b522ea05c
kronhyx <kronhyx@gmail.com>
2020-06-09 15:55:14 -0300

    feat: add main controller

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class IndexController
+ * @package App\Controller
+ */
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
