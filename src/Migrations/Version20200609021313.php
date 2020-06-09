1144dcae81f3198ccadea365be721eed0fbac472
kronhyx <kronhyx@gmail.com>
2020-06-08 23:18:53 -0300

    refactor: initial doctrine migration

--- src/Migrations/Version20200609021313.php
+++ src/Migrations/Version20200609021313.php
@@ -8,14 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Class Version20200609004947
+ * Class Version20200609021313
  * @package App\Migrations
  */
-final class Version20200609004947 extends AbstractMigration
+final class Version20200609021313 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Add products and tags with relationsip to each other';
+        return 'Add Product and Tag entities';
     }
 
     public function up(Schema $schema) : void
@@ -24,8 +24,7 @@ final class Version20200609004947 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(64) NOT NULL, price DOUBLE PRECISION NOT NULL, description LONGTEXT DEFAULT NULL, image LONGTEXT NOT NULL, stock INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, product_id INT DEFAULT NULL, name VARCHAR(64) NOT NULL, INDEX IDX_389B7834584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE tag ADD CONSTRAINT FK_389B7834584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(64) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -33,7 +32,6 @@ final class Version20200609004947 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE tag DROP FOREIGN KEY FK_389B7834584665A');
         $this->addSql('DROP TABLE product');
         $this->addSql('DROP TABLE tag');
     }
