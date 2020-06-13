dfdcd9766cde59b48d21ec9008e2bea8fa62aa7c
kronhyx <kronhyx@gmail.com>
2020-06-13 12:34:03 -0300

    refactor: jquery responsive

--- assets/js/sb-admin-2.js
+++ assets/js/sb-admin-2.js
@@ -2,6 +2,17 @@
 import 'jquery.easing'
 import 'multiselect'
 
+// Toggle the side navigation
+$("#sidebarToggle, #sidebarToggleTop").on('click', () => {
+	const sidebar = $(".sidebar")
+
+	$("body").toggleClass("sidebar-toggled");
+	sidebar.toggleClass("toggled");
+	if (sidebar.hasClass("toggled")) {
+		$('.sidebar .collapse').collapse('hide');
+	}
+});
+
 // Close any open menu accordions when window is resized below 768px
 $(window).resize(() => {
 	if ($(window).width() < 768) {
@@ -12,20 +23,11 @@ $(window).resize(() => {
 // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
 $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
 	if ($(window).width() > 768) {
-		var e0    = e.originalEvent,
-		    delta = e0.wheelDelta || -e0.detail;
+		const e0    = e.originalEvent,
+		      delta = e0.wheelDelta || -e0.detail
 		this.scrollTop += (delta < 0 ? 1 : -1) * 30;
 		e.preventDefault();
 	}
-});
-
-// Smooth scrolling using jQuery easing
-$(document).on('click', 'a.scroll-to-top', function (e) {
-	var $anchor = $(this);
-	$('html, body').stop().animate({
-		scrollTop: ($($anchor.attr('href')).offset().top)
-	}, 1000, 'easeInOutExpo');
-	e.preventDefault();
 });
 
 $(document).ready(() => {
